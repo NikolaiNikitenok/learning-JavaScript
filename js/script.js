68586862aa9e054772f7690273a2221b8e1caeec
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2022-08-29 07:25:37 +0300

    .

---
+++ js/script.js
@@ -0,0 +1,65 @@
+"use strict";
+// Comment
+// alert('Hallo World!');
+
+/* Comment */
+// let a = 'Nikolai', b = 'Nikitenok';
+// alert(a, b);
+// alert(b);
+
+// let str1 = 'hello';
+// let str2 = ' world';
+// alert(str1.length + str2);
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = `aaa ${str1} bbb ${str2} ccc`;
+// alert(txt) ;
+
+// let str = `a
+// b
+// c`;
+// alert(str);
+
+// alert('abc'*'cde');
+// alert(1/0);
+// console.log(-1/0);
+
+const PI = 3.14;
+let r = 10;
+console.log(2 * PI * r);
+
+// let a = '5' * '1' + '2' * '1';
+// alert(a);
+
+// let a = '2', b = '3';
+// alert(a + b);
+
+// let a = '2', b = '3';
+// console.log(Number(a) + Number(b));
+
+// console.log( '2' + Number('3') );
+
+// let a = '5.5px', b = '6.25px';
+// console.log(parseFloat(a) + parseFloat(b) + 'px');
+
+// let num1 = 123, num2 = 345;
+// let sum = (String(num1) + String(num2));
+// console.log(sum.length);
+
+// let test = (3 * 'abc');
+// alert(test);
+
+// костыльный метод переворота строки
+// let str = "abcde";
+// let new_str = str[4] + str[3] + str[2] + str[1] + str[0];
+// alert(new_str);
+
+// Вывод символа с номером переменной
+// let str = "asdfgh";
+// let num = 3;
+// alert(str[num]);
+
+// Вывод последнего символа
+// let str = 'asdfghjlk';
+// console.log(str[str.length - 1]);
